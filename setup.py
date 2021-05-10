4100366e58ab911220779d8a598e9ebb87c55a73
Jash Karani <karanijash@gmail.com>
2021-05-10 21:07:01 +0530

    created gitignore + setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import sys
+from cx_Freeze import setup, Executable
+
+
+# GUI applications require a different base on Windows (the default is for
+# a console application).
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+setup(
+    name = "Pogulator",
+    version = "0.1",
+    description = "A calculator that contains trigo and quadratic solving features!",
+    options = {"build_exe": build_exe_options},
+    executables = [Executable("Calculatoy.py", base=base)]
+)
